5b1f8b404358ad7d7e9989a5bc44ea35bf3c4c2f
h1zardian <amanb.singh21@gmail.com>
2025-01-23 21:10:48 +0530

    testing build.sh Permission denied fix

--- build.sh
+++ build.sh
@@ -26,6 +26,7 @@ rpm-ostree install \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+
 /ctx/desktop-packages.sh
 
 #### Example for enabling a System Unit File
